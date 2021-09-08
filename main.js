79c91e27c2c915660e8daa8cebede32d00c5c3ea
tomastom100 <86069280+tomastom100@users.noreply.github.com>
2021-09-08 12:41:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var images = ["fam.jpeg","abu.jpeg", "ma.jpeg" , "pa.jpeg", "my brother.jpg", "yo.jpeg"];
+var names = ["Family","Nina", "Lili", "Juan", "Sebas", "Tomas"];
+var i = 0;
+function update()
+{
+  document.getElementById("family_member_image").src=images[i];
+  document.getElementById("family_member_name").innerHTML=names[i];
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
